9e57cb2a113dbaa1e7ee6bf0a1d111b73bf49e19
wangxuxin <a1274745271@live.com>
2017-11-19 19:43:01 +0800

    serial debug

--- PersonalSmartCupServer/PSC/Hardware.py
+++ PersonalSmartCupServer/PSC/Hardware.py
@@ -4,16 +4,19 @@ weight = 0
 temp1 = 0
 temp2 = 0
 
+
 def startSerial():
     serial = Serial()
     serial.__init__()
     temp = ""
-    cmd = ""
     while True:
         temp += serial.read_all()
         cmd = temp.split("\n")
         temp = cmd[len(cmd) - 1]
         for i in cmd:
+            global weight
+            global temp1
+            global temp2
             t = i.split(":")
             if t[0] == "weight":
                 weight = t[1]
@@ -22,3 +25,10 @@ def startSerial():
             elif t[0] == "temp2":
                 temp2 = t[1]
 
+
+if __name__ == '__main__':
+    startSerial()
+    while True:
+        print('weight:?', weight)
+        print('temp1:?', temp1)
+        print('temp2:?', temp2)
