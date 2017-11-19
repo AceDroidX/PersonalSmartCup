bbf47f816f5a33f3c65371d22cca442c79893559
wangxuxin <a1274745271@live.com>
2017-11-19 19:35:38 +0800

    add serial

--- PersonalSmartCupServer/PSC/Hardware.py
+++ PersonalSmartCupServer/PSC/Hardware.py
@@ -1,12 +1,24 @@
-import Main
-from pcduino import adc
+from pcduino.myserial import *
 
+weight = 0
+temp1 = 0
+temp2 = 0
 
-def getT():
-    return adc.analog_read(Main.pinList['t'])
-
-
-def getP():
-    return adc.analog_read(Main.pinList['p'])
-
+def startSerial():
+    serial = Serial()
+    serial.__init__()
+    temp = ""
+    cmd = ""
+    while True:
+        temp += serial.read_all()
+        cmd = temp.split("\n")
+        temp = cmd[len(cmd) - 1]
+        for i in cmd:
+            t = i.split(":")
+            if t[0] == "weight":
+                weight = t[1]
+            elif t[0] == "temp1":
+                temp1 = t[1]
+            elif t[0] == "temp2":
+                temp2 = t[1]
 
